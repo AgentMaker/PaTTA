7aee7452af5b51abc749389af844e3ea438aae5c
Zhihao Cao <1564758299@qq.com>
2021-03-13 12:09:10 +0800

    Update load_model.py

--- patta/load_model.py
+++ patta/load_model.py
@@ -1,5 +1,6 @@
 import paddle
 
+# Related to https://www.paddlepaddle.org.cn/documentation/docs/zh/api/paddle/fluid/dygraph/jit/load_cn.html#load
 def load_model(path='output/model'):
     model = paddle.jit.load(path=path)
 
